871ec751287095692135507e5833e7f2f81ea819
tanya58 <123757194+tanya58@users.noreply.github.com>
2024-10-08 23:12:03 +0530

    Time: 104 ms (87.75%), Space: 17.9 MB (22.22%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,11 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        l=len(nums)
-        s=sum(nums)
-        return l*(l+1)/2-s
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        v = [-1] * (n + 1)
+        for num in nums:
+            v[num] = num
+        for i in range(len(v)):
+            if v[i] == -1:
+                return i
+        return 0
         
